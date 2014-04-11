45f821e1b8e4f183ff73a6f851e7bb31f4e84417
Sam Marks <sam@sammarks.me>
2014-04-10 23:16:48 -0400

    Update documentation and add getModule() function to get the name of the current module.

--- lib/AbleCore/Modules/ThemeManager.php
+++ lib/AbleCore/Modules/ThemeManager.php
@@ -54,6 +54,14 @@ class ThemeManager
 	private $generated = array();
 
 	/**
+	 * Module
+	 *
+	 * The name of the current module.
+	 * @var bool|null
+	 */
+	private $module = null;
+
+	/**
 	 * Init
 	 *
 	 * Creates a new ThemeManager.
@@ -70,21 +78,21 @@ class ThemeManager
 	 *
 	 * Adds a new theme hook definition to the ThemeManager.
 	 *
-	 * Example paths:
-	 *
-	 * - path => `themes/path.suffix`
-	 * - folder.path => `themes/folder/path.suffix`
-	 * - folder.subfolder.item => `themes/folder/subfolder/item.suffix`
-	 *
 	 * @param  string $key                      The key for the theme hook.
 	 * @param  array  $additional_configuration An array of additional configuration options.
 	 *
+	 * @throws \Exception
 	 * @return ThemeManager
 	 */
 	public function define($key, $additional_configuration = array())
 	{
 		// Generate the template path.
 		$generated_path = str_replace('_', '-', $key);
+
+		$module = $this->getModule();
+		if (!$module) {
+			throw new \Exception('A module name could not be found when adding the theme. You might have to specify it manually.');
+		}
 
 		$this->generated[$key] = array(
 			'template' => $generated_path,
@@ -94,7 +102,7 @@ class ThemeManager
 			// as an Able Core theme so that we can perform some extra file-checking
 			// magic inside hook_theme_registry_alter. The variable is removed inside
 			// hook_theme_registry_alter.
-			'variables' => array('ablecore' => 'yes'),
+			'variables' => array('ablecore' => array('module' => $module))
 		);
 
 		$this->generated[$key] = array_replace_recursive($this->generated[$key], $additional_configuration);
@@ -113,6 +121,33 @@ class ThemeManager
 	public function fin()
 	{
 		return $this->generated;
+	}
+
+	/**
+	 * getModule
+	 *
+	 * Searches through the PHP Debug Backtrace to get the name of the module
+	 * currently calling the Able Core libraries. This is so that developers
+	 * don't have to specify the module name every time they invoke the libraries.
+	 *
+	 * This seems like the best way to do it with the tools available.
+	 *
+	 * @return bool|string The name of the module if successful, false otherwise.
+	 */
+	protected function getModule()
+	{
+		if ($this->module !== null) return $this->module;
+		$backtrace = debug_backtrace();
+		foreach ($backtrace as $item) {
+			if (!array_key_exists('function', $item)) continue;
+			$segments = explode('_', $item['function']);
+			if (count($segments) <= 1) continue;
+			if ($segments[count($segments) - 1] == 'theme') {
+				$module_segments = array_slice($segments, 0, count($segments) - 1);
+				return $this->module = implode('_', $module_segments);
+			}
+		}
+		return $this->module = false;
 	}
 
 }
