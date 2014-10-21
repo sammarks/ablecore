22f3ff335025814bb33913994a75b5dccba0e123
Sam Marks <sam@sammarks.me>
2014-10-20 20:38:26 -0400

    Add the tree by vocabulary function.

--- lib/AbleCore/TaxonomyTerm.php
+++ lib/AbleCore/TaxonomyTerm.php
@@ -33,4 +33,57 @@ class TaxonomyTerm extends EntityExtension {
 		return static::mapQuery($query);
 	}
 
+	/**
+	 * Tree by Vocabulary
+	 *
+	 * @param string $vocabulary_machine_name The vocabulary machine name to get the
+	 *                                        taxonomy term tree for.
+	 *
+	 * @return array A better (heirarchical) array of taxonomy terms.
+	 */
+	public static function treeByVocabulary($vocabulary_machine_name)
+	{
+		$tree = &drupal_static(__FUNCTION__ . ':' . $vocabulary_machine_name, null);
+		if ($tree === null) {
+
+			// Load the vocabulary.
+			$vocabulary = taxonomy_vocabulary_machine_name_load($vocabulary_machine_name);
+			if (!$vocabulary) {
+				return array();
+			}
+
+			// Get the original, flat tree.
+			$flat_tree = taxonomy_get_tree($vocabulary->vid);
+
+			// Get the new heirarchical tree of terms.
+			$tree = static::getTermTree($flat_tree);
+
+		}
+
+		return $tree;
+	}
+
+	/**
+	 * Get Term Tree
+	 *
+	 * @param array $flat_tree The flat tree returned by taxonomy_get_tree()
+	 * @param int   $parent_id The TID of the parent to get children for.
+	 *
+	 * @return array A heirarchical array of taxonomy terms, as configured in
+	 *               the taxonomy term administration.
+	 */
+	protected static function getTermTree(array $flat_tree, $parent_id = 0)
+	{
+		$terms = array();
+		foreach ($flat_tree as $term) {
+			if (array_search($parent_id, $term->parents) !== false) {
+				$terms[$term->tid] = $term;
+			}
+			if ($children = static::getTermTree($flat_tree, $term->tid)) {
+				$terms[$term->tid]->children = $children;
+			}
+		}
+		return $terms;
+	}
+
 }
