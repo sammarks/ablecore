1625067a98206054d5d82ce9dacc7937d748f2a9
Sam Marks <sam@sammarks.me>
2014-12-15 13:06:11 -0500

    Make the entity reference field like the others.

--- lib/AbleCore/Install/Helpers/Fields/EntityReferenceField.php
+++ lib/AbleCore/Install/Helpers/Fields/EntityReferenceField.php
@@ -31,45 +31,20 @@ class EntityReferenceField extends Field {
 	/**
 	 * Defines the bundles this field references.
 	 *
-	 * @param array $bundles An array of bundles this entity reference references.
+	 * @param array  $bundles An array of bundles this entity reference references.
+	 * @param string $type    The type of entity to reference.
 	 *
 	 * @return $this
 	 */
-	public function references(array $bundles = array())
+	public function references(array $bundles = array(), $type = 'node')
 	{
+		$this->setSetting('target_type', $type);
 		$this->definition['settings']['handler_settings']['target_bundles'] = drupal_map_assoc($bundles);
 		return $this;
 	}
 
-	/**
-	 * Sets the entity type this field references.
-	 *
-	 * @param string $entity_type The entity type.
-	 *
-	 * @return $this
-	 */
-	public function referencesType($entity_type = 'node')
+	public function setType($type = FieldTypes::ENTITY_REFERENCE)
 	{
-		return $this->setSetting('target_type', $entity_type);
-	}
-
-	/**
-	 * Creates a new entity reference field.
-	 *
-	 * @param string $field_name  The name of the new field.
-	 * @param array  $bundles     The bundles the field references (must not be empty).
-	 * @param string $entity_type The type of entity the field references.
-	 *
-	 * @return $this
-	 * @throws \Exception
-	 */
-	public static function createEntityReference($field_name, array $bundles = array(), $entity_type = 'node')
-	{
-		$instance = static::create($field_name);
-		$instance->setType(FieldTypes::ENTITY_REFERENCE);
-		$instance->references($bundles);
-		$instance->referencesType($entity_type);
-
-		return $instance->save();
+		return parent::setType($type);
 	}
 }
