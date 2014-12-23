efd1bd3818eb05e42a5fdcc4688d4ea8f408a3f6
Sam Marks <sam@sammarks.me>
2014-12-23 15:08:42 -0500

    Don't trigger an error if the field doesn't exist. Just return false.

--- lib/AbleCore/Entity.php
+++ lib/AbleCore/Entity.php
@@ -297,7 +297,6 @@ class Entity extends DrupalExtension {
 				return parent::__get($name);
 			} catch (\Exception $ex) {
 				if ($this->full_loaded) {
-					trigger_error('The field ' . $name . ' doesn\'t exist.');
 					return false;
 				} else {
 					// If we don't already have the full entity object, load it and try to get the field again.
