2fd5cef12c252305038463233188f67ebfc4de79
Sam Marks <sam@sammarks.me>
2015-06-11 10:51:47 -0400

    Add number() and update the link() method to use it.

--- lib/AbleCore/Fields/FieldValueTypes/PhoneNumberFieldValue.php
+++ lib/AbleCore/Fields/FieldValueTypes/PhoneNumberFieldValue.php
@@ -6,9 +6,22 @@ use AbleCore\Fields\FieldValue;
 
 class PhoneNumberFieldValue extends FieldValue {
 
+	/**
+	 * Generates an iPhone-friendly link to the telephone number.
+	 * @return string
+	 */
 	public function link()
 	{
-		return l($this->value, 'tel:' . $this->value);
+		return l($this->value, 'tel:' . $this->number());
+	}
+
+	/**
+	 * Generates an iPhone-friendly version of the telephone number.
+	 * @return mixed
+	 */
+	public function number()
+	{
+		return str_replace(' x', ',', $this->value);
 	}
 
 }
