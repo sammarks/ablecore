46e2f63f0c1d97aca952c5afa7599f1c41d1e1c5
Sam Marks <sam@sammarks.me>
2015-12-07 15:25:50 -0500

    Require helper files recursively.

--- lib/AbleCore/Internal/HookUtils.php
+++ lib/AbleCore/Internal/HookUtils.php
@@ -14,13 +14,26 @@ class HookUtils {
 		return function_exists($module . '_ablecore');
 	}
 
-	public static function includeDirectory($module, $directory)
+	public static function includeDirectory($module, $directory, $recursive = true)
 	{
 		$module_path = drupal_get_path('module', $module);
 		$directory_path = DRUPAL_ROOT . '/' . $module_path . '/' . $directory;
 		if (is_dir($directory_path)) {
-			foreach (glob($directory_path . '/**.inc') as $file) {
-				require_once $file;
+			if ($recursive) {
+				$dir = new \RecursiveDirectoryIterator($directory_path);
+				$ite = new \RecursiveIteratorIterator($dir);
+				$files = new \RegexIterator($ite, '%^.*\.inc$%', \RegexIterator::GET_MATCH);
+				foreach ($files as $sub_files) {
+					foreach ($sub_files as $file) {
+						if (file_exists($file)) {
+							require_once $file;
+						}
+					}
+				}
+			} else {
+				foreach (glob($directory_path . '/**.inc') as $file) {
+					require_once $file;
+				}
 			}
 		}
 	}
